88e809a77f2897139957bd3bea0b5733260a7fc4
Jehan Zaib <jehanzaibalikhan@yahoo.com>
2024-10-27 11:32:48 +0500

    updated dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -51,7 +51,7 @@ else:
     # Load the models
     gbc_model = load_model('GradientBoostingClassifier1')
     lightgbm_model = load_model('LGBMClassifier2')
-    rf_model = load_model('RandomForestClassifier3.pkl')
+    rf_model = load_model('RandomForestClassifier3')
 
     # Streamlit app interface
     st.markdown("<h1 style='text-align: center; color: black;'>Instructor Dashboard for E-Learning Systems</h1>", unsafe_allow_html=True)
