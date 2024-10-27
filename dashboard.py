620e3b1e86ef3b5597dd68af61225239240cf368
Jehan Zaib <jehanzaibalikhan@yahoo.com>
2024-10-27 10:57:51 +0500

    update dashboard.py and requirements.txt

--- dashboard.py
+++ dashboard.py
@@ -4,6 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 from pycaret.classification import *
+import joblib
 
 # Dummy credentials for instructors
 valid_users = {"mehboobali": "123", "jehanzaib": "456"}
